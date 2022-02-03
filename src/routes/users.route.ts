0b6061fae2667682fa8a49e946f04ebcd143eefd
Alexandre Moreira <4lexandre.moreira@gmail.com>
2022-02-03 14:20:25 -0300

    fix: created crud users

---
+++ src/routes/users.route.ts
@@ -0,0 +1,42 @@
+import { Router, Request, Response, NextFunction } from "express";
+import { StatusCodes } from 'http-status-codes';
+
+const userRoute = Router();
+
+userRoute.get('/users', (req: Request, res: Response, next: NextFunction) =>{
+    const users = [{userName: 'Alexandre'}];
+    res.status(StatusCodes.OK).send(users);
+});
+
+userRoute.get('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction) =>{
+    const uuid = req.params.uuid;
+    res.status(StatusCodes.OK).send({uuid});
+} )
+
+userRoute.post('/users', (req: Request, res: Response, next: NextFunction) =>{
+    const newUser = req.body;
+    console.log(req.body);
+    res.status(StatusCodes.CREATED).send(newUser);
+})
+
+userRoute.put('/users/:uuid',(req: Request<{uuid: string}>, res: Response, next: NextFunction) =>{
+    const uuid = req.params.uuid;
+    const modifieUser = req.body;
+
+    modifieUser.uuid = uuid;
+
+    res.status(StatusCodes.OK).send(modifieUser);
+});
+
+userRoute.delete('/users/:uuid',(req: Request<{uuid: string}>, res: Response, next: NextFunction) =>{
+    res.sendStatus(StatusCodes.OK)
+});
+
+export default userRoute;
+
+
+// get /users
+// get /users/:uuid
+// post /users
+// put /users/:uuid
+// delete /users/:uuid
