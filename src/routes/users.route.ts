6abb4dbef64f3971d46645e3c56abbdca98122e6
Alexandre Moreira <4lexandre.moreira@gmail.com>
2022-02-03 17:21:54 -0300

    Fix: Create user search in database

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -9,9 +9,10 @@ userRoute.get('/users', async (req: Request, res: Response, next: NextFunction) 
     res.status(StatusCodes.OK).send(users);
 });
 
-userRoute.get('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction) =>{
+userRoute.get('/users/:uuid', async (req: Request<{uuid: string}>, res: Response, next: NextFunction) =>{
     const uuid = req.params.uuid;
-    res.status(StatusCodes.OK).send({uuid});
+    const user = await userRepository.findById(uuid);
+    res.status(StatusCodes.OK).send({user});
 } )
 
 userRoute.post('/users', (req: Request, res: Response, next: NextFunction) =>{
